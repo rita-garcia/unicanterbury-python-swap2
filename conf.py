1ac0035a274170fd68932810502fb139e95dd6d0
Rita Garcia <rita.garcia@unity3d.com>
2023-09-26 17:39:34 +1300

    Changing the references from python-swap2 to uc-python-swap2

--- conf.py
+++ conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'python-swap2'
+project = 'uc-python-swap2'
 copyright = '2023 - Barbara Ericson'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -122,7 +122,7 @@ html_theme = 'sphinx_bootstrap'
 #html_theme_options = {'nosidebar': 'true'}
 html_theme_options = {
     # Navigation bar title. (Default: ``project`` value)
-    'navbar_title': "python-swap2",
+    'navbar_title': "uc-python-swap2",
 
     # Tab name for entire site. (Default: "Site")
     'navbar_site_name': "Chapters",
@@ -170,10 +170,10 @@ html_theme_path = [pkg_resources.resource_filename('runestone', 'common/project_
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'python-swap2'
+html_title = 'uc-python-swap2'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title ='python-swap2'
+html_short_title ='uc-python-swap2'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
